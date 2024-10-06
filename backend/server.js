28f80b85741400a5b4f2ff588dd2277349ad4cf5
jerrydumas <dumas.jerry@gmail.com>
2024-10-06 16:42:59 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,42 @@
 import express from 'express'
+import cors from 'cors'
+import { MongoClient } from 'mongodb'
 
 const app = express()
 const port = process.env.PORT || 4000
 
+app.use(cors())
 app.use(express.json())
 
+// Connection URL
+const url = process.env.DBCONNECT;
+const client = new MongoClient(url);
+
+
+// Database Name
+const dbName = 'AmazingMernApp';
+
+async function main() {
+	
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to server');
+  const db = client.db(dbName);
+  const collection = db.collection('animals');
+  return 'done.';
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
 app.get('/', (req,res)=>{
     res.send('Sup World')
 })
-
+app.get('/animals', await (req,res)=>{
+const findResult = await collection.find({}).toArray();
+console.log('Found documents =>', findResult);
+})
 app.listen(port, ()=>{
     `App listening on port ${port}`
 })
