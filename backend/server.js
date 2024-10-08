dc7ba561dbbc726fe440615044632b4ce65be6e1
jerrydumas <dumas.jerry@gmail.com>
2024-10-08 17:17:54 -0400

    2nd update dbconnect.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import cors from 'cors'
+import clientPromise from './dbconnect.js'
+
 
 const app = express()
 const port = process.env.PORT || 4000
@@ -12,8 +14,21 @@ app.get('/', (req,res)=>{
 })
 app.get('/animals', async (req,res)=>{
 const findResult = await collection.find({}).toArray();
-console.log('Found documents =>', findResult);
+console.log('Found documents =>', findResult);  
+
 })
 app.listen(port, ()=>{
     `App listening on port ${port}`
 })
+
+
+export async function main(){
+  const client = await clientPromise
+  // const isConnected = await client.isConnected()
+  const db = client.db('AmazingMernApp')
+  const collection = db.collection('animals')
+}
+main()
+.then(console.log)
+.catch(console.error)
+.finally(()=>client.close())
