cc1e10641a3a7957ef5a5eb07cddaa1daad7053c
jerrydumas <dumas.jerry@gmail.com>
2024-10-08 16:57:58 -0400

    update dbconnect.js

--- backend/dbconnect.js
+++ backend/dbconnect.js
@@ -5,3 +5,19 @@ const options = {
     useUnifiedTopology:true,
     useNewURLParser:true
 }
+let client
+let clientPromise
+
+if(!process.env.DBCONNECT){
+    throw new ERROR('Please add your DBCONNECT Stringto .env local')
+}
+if(process.env.DBCONNECT == 'development'){
+    if(!global._MongoClientPromise){
+        client = new MongoClient(uri,options)
+        global._MongoClientPromise = client.connect()
+    }else{
+        client= new MongoClient(uri,options)
+        clientPromise = new client.connect()
+    }
+}
+export default clientPromise
