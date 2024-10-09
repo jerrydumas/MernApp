e282d58dbe5ff9edb76fcf8477c165ff76d4e5ca
jerrydumas <dumas.jerry@gmail.com>
2024-10-09 18:49:08 -0400

    3rd database update

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import { MongoClient } from 'mongodb'
 const app = express()
 const port = process.env.PORT || 4000
 const uri = process.env.DBCONNECT
-let db = "AMAZINGMERNAPP"
+let db = "AmazingMernApp"
 let collection = "animals"
 
 app.use(cors())
@@ -21,8 +21,6 @@ console.log('Found documents =>', findResult);
   } catch (error) {
     console.log(error)
   }
-  
-
 })
 app.listen(port, ()=>{
     `App listening on port ${port}`
